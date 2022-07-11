12c1f76742fb944e3d0a5e5981ac2692b156e383
Francis <50196946+Basharfrancis@users.noreply.github.com>
2022-07-11 14:22:26 -0300

    initial skeleton commit (#1)
    * initial skeleton commit
    
    * clean
    
    * clean
    
    * clean
    
    * hello world
    
    * clean
    
    * clean
    
    * clean
    
    * remove husky folder
    
    * clean
    
    * clean
    
    * Hello world
    
    * remove husky
    
    * update readme
    
    * add package
    
    Co-authored-by: Basharfrancis <bashar.francis@rafflebox.ca>

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+interface Response {
+  statusCode: number;
+  headers?: Record<string, string>;
+  body?: string;
+}
+
+const handler = async function (event: { path: string }): Promise<Response> {
+  console.log('request:', JSON.stringify(event, undefined, 2));
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: `Hello, World! You've hit ${event.path}\n`,
+  };
+};
+
+export { handler };
