13db526c986145c15ae506350cec7898b853f22d
Basharfrancis <bashar.francis@rafflebox.ca>
2022-07-11 12:37:11 -0300

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-let message: string = 'Hello, World!'
-console.log(message)
+interface Response {
+  statusCode: number
+  headers?: Record<string, string>
+  body?: string
+}
+
+const handler = async function (event: { path: string }): Promise<Response> {
+  console.log('request:', JSON.stringify(event, undefined, 2))
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: `Hello, CDK! You've hit ${event.path}\n`,
+  }
+}
+
+export { handler }
