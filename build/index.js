143540f1122f68aaf8ac45dbd6136145a748ed46
Basharfrancis <bashar.francis@rafflebox.ca>
2022-07-11 10:19:49 -0300

    clean

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let message = 'Hello, World!';
-console.log(message);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksT0FBTyxHQUFXLGVBQWUsQ0FBQTtBQUNyQyxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxDQUFBIn0=
