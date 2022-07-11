c51a4121c9f6af1935176e300b00a9848e693b1b
Basharfrancis <bashar.francis@rafflebox.ca>
2022-07-11 13:43:50 -0300

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 interface Response {
-  statusCode: number
-  headers?: Record<string, string>
-  body?: string
+  statusCode: number;
+  headers?: Record<string, string>;
+  body?: string;
 }
 
 const handler = async function (event: { path: string }): Promise<Response> {
-  console.log('request:', JSON.stringify(event, undefined, 2))
+  console.log('request:', JSON.stringify(event, undefined, 2));
+
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/plain' },
     body: `Hello, CDK! You've hit ${event.path}\n`,
-  }
-}
+  };
+};
 
-export { handler }
+export { handler };
